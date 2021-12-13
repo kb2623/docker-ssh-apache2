c85be22cd23676b2f9ddee148068e45a4bd4ef36
Klemen@MkS2ExE <roxor1992@gmail.com>
2021-12-13 13:16:40 +0100

    Added multiple files upload

--- files/index.php
+++ files/index.php
@@ -10,7 +10,7 @@
   <br><br>
   Priimek: <input type="text" name="priimek">
   <br><br>
-  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="file" name="fileToUpload" id="fileToUpload" multiple>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
--- files/upload_form.php
+++ files/upload_form.php
@@ -22,16 +22,37 @@ if (empty($ime) || empty($priimek)) {
 	exit("Obvezno vnesi ime in priimek!");
 }
 
-$target_dir = $ime . "_" . $priimek . "/";
-
-mkdir($target_dir, 0777, true);
-
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-	echo "The file <strong>". htmlspecialchars(basename( $_FILES["fileToUpload"]["name"])). "</strong> has been uploaded to <strong>" . $target_dir . "</strong>.<br>";
+// Checks if user sent an empty form
+if(!empty(array_filter($_FILES['fileToUpload']['name']))) {
+	$target_dir = $ime . "_" . $priimek . "/";
+	mkdir($target_dir, 0777, true);
+	// Loop through each file in files[] array
+	foreach ($_FILES['fileToUpload']['tmp_name'] as $key => $value) {
+		$file_tmpname = $_FILES['fileToUpload']['tmp_name'][$key];
+		$file_name = $_FILES['fileToUpload']['name'][$key];
+		// Set upload file path
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		// If file with name already exist then append time in
+		// front of name of the file to avoid overwriting of file
+		$file_ext = pathinfo($target_file, PATHINFO_EXTENSION);
+		if(file_exists($filepath)) {
+			$filepath = $target_dir.time() . $file_name;
+			if(move_uploaded_file($file_tmpname, $target_file)) {
+				echo "{$file_name} successfully uploaded <br />";
+			} else {
+				echo "Error uploading {$file_name} <br />";
+			}
+		} else {
+			if( move_uploaded_file($file_tmpname, $target_file)) {
+				echo "{$file_name} successfully uploaded <br />";
+			} else {                    
+				echo "Error uploading {$file_name} <br />";
+			}
+		}
+	}
 } else {
-	exit("Sorry, there was an error uploading your file.");
+	// If no files selected
+	echo "No files selected.";
 }
 
 echo "<a href='index.php'><button>Return</button></a><br>"
